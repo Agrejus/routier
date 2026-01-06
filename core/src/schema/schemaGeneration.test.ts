c3ef3d816e014ff4af0e04de22b3a8840d8e75f5
James DeMeuse <james.demeuse@gmail.com>
2026-01-06 08:55:14 -0600

    Fix async

--- core/src/schema/schemaGeneration.test.ts
+++ core/src/schema/schemaGeneration.test.ts
@@ -1,4 +1,4 @@
-import { describe, it } from '@jest/globals';
+import { describe, it, expect } from '@jest/globals';
 import { factories } from './testSchemas.test';
 import { s } from './builder';
 
